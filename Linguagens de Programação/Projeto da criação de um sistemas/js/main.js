74074c190c25e3095519cb7a163b0e9005e22c06
Luiz Fernando <66912726+Luiz-Fernando-sys@users.noreply.github.com>
2021-11-20 14:31:37 -0300

    Projeto Super Heróis
    Projeto referente à criação de um site sobre super heróis, ao qual foi utilizado BootStrap no FrontEnd, bem como HTML, CSS, e JavaScript puro também em arquivos separados.

---
+++ Linguagens de Programação/Projeto da criação de um sistemas/js/main.js
@@ -0,0 +1,25 @@
+//console.log("Hello World!!!");
+//alert("Hello World!!!");
+
+var tagTitle = document.querySelector("title");
+var tagH1 = document.querySelector("h1");
+
+var title = tagTitle.textContent;
+var h1 = tagH1.textContent;
+
+
+//alert(title); 
+console.log(title);
+
+//alert(h1);
+console.log(h1);
+
+
+var trSegundoCliente = document.querySelector(".segundo-cliente");
+var segundoCliente = trSegundoCliente.querySelector("td");
+var nomeSegundoCliente = segundoCliente.textContent;
+
+nomeSegundoCliente = "Luiz Fernando Adami";
+
+alert(nomeSegundoCliente);
+console.log(nomeSegundoCliente);
